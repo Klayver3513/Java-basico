81fc1e48c1ab0192be38d6b8e607c9f20a6960a6
Klayver de Almeida <klayversoares9@gmail.com>
2024-04-26 13:30:42 -0300

    operadores

---
+++ bootcamp skills/src/edu/klayver/semanaUm/Operadores.java
@@ -0,0 +1,15 @@
+package edu.klayver.semanaUm;
+
+public class Operadores {
+    public static void main(String[] args) {
+        int a,b;
+        a=5;
+        b=6;
+        String resultado= "";
+        if(a==b)
+        resultado="true";
+        else 
+        resultado="false";
+        System.out.println(resultado);
+    }
+}
